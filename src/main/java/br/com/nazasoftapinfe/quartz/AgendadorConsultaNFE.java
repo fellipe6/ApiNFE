2ba388290dbeca517ae1383611bc9e74f4685c04
psouza <CrioRad123@1>
2023-04-14 15:57:25 -0300

    Ajustes em DistribuicaoService

--- src/main/java/br/com/nazasoftapinfe/quartz/AgendadorConsultaNFE.java
+++ src/main/java/br/com/nazasoftapinfe/quartz/AgendadorConsultaNFE.java
@@ -1,5 +1,6 @@
 package br.com.nazasoftapinfe.quartz;
 
+import br.com.nazasoftapinfe.controller.NotaEntradaController;
 import br.com.nazasoftapinfe.service.DistribuicaoService;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.DisallowConcurrentExecution;
@@ -13,14 +14,17 @@ public class AgendadorConsultaNFE {
 
     private final DistribuicaoService distribuicaoService;
 
-    public AgendadorConsultaNFE(DistribuicaoService distribuicaoService) {
+    private final NotaEntradaController notaEntradaController;
+
+    public AgendadorConsultaNFE(DistribuicaoService distribuicaoService, NotaEntradaController notaEntradaController) {
         this.distribuicaoService = distribuicaoService;
+        this.notaEntradaController = notaEntradaController;
     }
-    @Scheduled(initialDelay=1000*60*10,fixedDelay=7200000)
+    @Scheduled(initialDelay=1000*60*10,fixedDelay=3600000)
     public void efetuaConsulta(){
         try {
             log.info("Iniciando a operação de consultas!");
-            distribuicaoService.consultaNotas();
+            notaEntradaController.consulta();
             log.info("Operação de consulta finalizada!");
         }catch (Exception e){
             log.error("Erro ao realizar a consulta de notas na SEFAZ!", e);
